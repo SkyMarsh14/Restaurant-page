997afc5702202b776af4e251ae30e15ef896c41f
Sky Marsh <soras1758@gmail.com>
2024-08-13 15:24:09 +0900

    set up npm packeges

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+import "./styles.css";
+import steakImage from "./images/medium-rare-steak.jpg";
+
+const image = document.createElement("img");
+image.src = steakImage;
+
+document.body.append(image);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,37 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports ={
+module.exports = { 
     mode:"development",
     entry:"./src/index.js",
     output:{
         filename:"main.js",
         path:path.resolve(__dirname,"dist"),
-        clean:true,
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/index.html"]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.css$/i,
+                use:["style-loader","css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test:/\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
 };
